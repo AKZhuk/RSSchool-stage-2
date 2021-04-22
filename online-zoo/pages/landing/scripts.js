a7b480e74d9065d8a918e0d9cfed8e1081ddb21a
Aliaksei Zhuk <lesha.zhuk@gmail.com>
2021-04-22 19:23:44 +0300

    feat: implement support changes theme

--- online-zoo/pages/landing/scripts.js
+++ online-zoo/pages/landing/scripts.js
@@ -1,11 +1,21 @@
 let checkbox = document.querySelector("input[name=theme]");
 let html = document.querySelector(".html");
+
 checkbox.addEventListener("change", function () {
   if (this.checked) {
+    localStorage.setItem("theme", "dark-theme");
     html.classList.remove("light-theme");
     html.classList.add("dark-theme");
+    let map = document.querySelector(".map__object").contentWindow.document.querySelector("svg");
+    map.classList.remove("light-theme");
+    map.classList.add("dark-theme");
   } else {
+    localStorage.setItem("theme", "light-theme");
     html.classList.add("light-theme");
     html.classList.remove("dark-theme");
+    let map = document.querySelector(".map__object").contentWindow.document.querySelector("svg");
+
+    map.classList.add("light-theme");
+    map.classList.remove("dark-theme");
   }
 });
