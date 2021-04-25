081e8ca66ff0acae07abf523a06fd8a8ff2295d6
Aliaksei Zhuk <lesha.zhuk@gmail.com>
2021-04-25 22:19:03 +0300

    fix: issue according to the requirement

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,11 +1,13 @@
-const baseImgUrl = "https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/";
-const fullScreen = document.querySelector(".fullscreen");
-const filters = document.querySelector(".filters");
-const buttonsContainer = document.querySelector(".btn-container");
-const fileImport = document.querySelector(".btn-load--input");
-const buttons = document.querySelectorAll(".btn");
-const canvas = document.querySelector("canvas");
-let filtersValue = {
+const baseImgUrl = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
+const canvas = document.querySelector('canvas');
+const displayImg = document.querySelector('img');
+const filters = document.querySelector('.filters');
+const buttonsContainer = document.querySelector('.btn-container');
+const fileImport = document.querySelector('.btn-load--input');
+const buttons = document.querySelectorAll('.btn');
+const fullScreen = document.querySelector('.fullscreen');
+
+const filtersState = {
   blur: 0,
   invert: 0,
   sepia: 0,
@@ -14,74 +16,74 @@ let filtersValue = {
   opacity: 100,
 };
 const images = [
-  "01",
-  "02",
-  "03",
-  "05",
-  "06",
-  "07",
-  "08",
-  "09",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-  "16",
-  "17",
-  "18",
-  "19",
-  "20",
+  '01',
+  '02',
+  '03',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
+  '10',
+  '11',
+  '12',
+  '13',
+  '14',
+  '15',
+  '16',
+  '17',
+  '18',
+  '19',
+  '20',
 ];
+
 window.onload = () => {
-  drawImage("assets/img/img.jpg");
+  drawImage('assets/img/img.jpg');
 };
 const img = new Image();
 let i = 0;
 
-filters.addEventListener("input", (e) => {
+filters.addEventListener('input', (e) => {
   e.target.nextElementSibling.value = e.target.value;
+  filtersState[e.target.name] = e.target.value;
   setStyle(e.target);
-  filtersValue[e.target.name] = e.target.value;
-  drawImage(img.src);
 });
 
-fileImport.addEventListener("change", (e) => {
+fileImport.addEventListener('change', (e) => {
   const file = fileImport.files[0];
   const reader = new FileReader();
+  setStatusActive(e.target.parentElement);
   reader.onload = () => {
     const img = new Image();
     img.src = reader.result;
     drawImage(img.src);
-    document.querySelector("img").setAttribute("src", img.src);
+    displayImg.src = img.src;
+    fileImport.value = null;
   };
   reader.readAsDataURL(file);
 });
 
-buttonsContainer.addEventListener("click", (e) => {
-  resetStatusActive();
-  setStatusActive(e.target);
-  if (e.target.classList.contains("btn-reset")) resetStyles();
-  if (e.target.classList.contains("btn-next")) getImage(e.target);
-  if (e.target.classList.contains("btn-save")) saveImage(e.target);
-});
-
 const setStyle = (filter) => {
   const unit = filter.dataset.sizing;
-
-  document.documentElement.querySelector("img").style.setProperty(`--${filter.name}`, filter.value + unit);
+  displayImg.style.setProperty(`--${filter.name}`, filter.value + unit);
+  drawImage(img.src);
 };
 
-const resetStyles = () => {
-  filters.querySelectorAll("input").forEach((filter) => {
-    filter.value = filter.getAttribute("value");
-    const output = filters.querySelector(`.${filter.name}-result`);
-    output.innerHTML = filter.getAttribute("value");
-    setStyle(filter);
-    filtersValue[filter.name] = filter.getAttribute("value");
-  });
+buttonsContainer.addEventListener('click', (e) => {
+  resetStatusActive();
+  setStatusActive(e.target);
+  if (e.target.classList.contains('btn-reset')) resetStyles();
+  if (e.target.classList.contains('btn-next')) getImage(e.target);
+  if (e.target.classList.contains('btn-save')) saveImage(e.target);
+});
 
+const resetStyles = () => {
+  filters.querySelectorAll('input').forEach((filter) => {
+    filter.value = filter.getAttribute('value');
+    filter.nextElementSibling.value = filter.value;
+    setStyle(filter);
+    filtersState[filter.name] = filter.value;
+  });
   drawImage(img.src);
 };
 
@@ -91,64 +93,69 @@ const getImage = (button) => {
   const img = new Image();
   img.src = baseImgUrl + timeOfDay + `/${images[i]}.jpg`;
   img.onload = () => {
-    document.querySelector("img").setAttribute("src", img.src);
+    displayImg.setAttribute('src', img.src);
   };
   i++;
   drawImage(img.src);
   button.disabled = true;
   setTimeout(() => {
     button.disabled = false;
-  }, 1000);
+  }, 500);
 };
 
 const saveImage = () => {
-  const link = document.createElement("a");
-  link.download = "download.png";
+  const link = document.createElement('a');
+  link.download = 'download.png';
   link.href = canvas.toDataURL();
   link.click();
   link.delete;
 };
 
 function drawImage(src) {
-  img.setAttribute("crossOrigin", "anonymous");
+  img.setAttribute('crossOrigin', 'anonymous');
   img.src = src;
   img.onload = function () {
     canvas.width = img.width;
     canvas.height = img.height;
-    const ctx = canvas.getContext("2d");
-    ctx.filter = `blur(${filtersValue.blur}px) invert(${filtersValue.invert}%) sepia(${filtersValue.sepia}%) saturate(${filtersValue.saturate}%) hue-rotate(${filtersValue.hue}deg)  opacity(${filtersValue.opacity}%)`;
+    const ctx = canvas.getContext('2d');
+
+    const coef =
+      img.window > img.height ? img.width / displayImg.clientWidth : img.height / displayImg.clientHeight;
+    ctx.filter = `blur(${filtersState.blur * coef}px) invert(${filtersState.invert}%) sepia(${
+      filtersState.sepia
+    }%) saturate(${filtersState.saturate}%) hue-rotate(${filtersState.hue}deg)  opacity(${
+      filtersState.opacity
+    }%)`;
     ctx.drawImage(img, 0, 0);
   };
 }
 
-drawImage();
-
 const getCurrentTime = () => {
   let currentHour = new Date().getHours();
   if ((timeOfDay = 6 < currentHour && currentHour < 12)) {
-    return "morning";
+    return 'morning';
   } else if (currentHour < 18) {
-    return "day";
+    return 'day';
   } else if (currentHour < 24) {
-    return "evening";
+    return 'evening';
   } else {
-    return "night";
+    return 'night';
   }
 };
 
 const resetStatusActive = () => {
   buttons.forEach((button) => {
-    button.classList.remove("btn-active");
+    button.classList.remove('btn-active');
   });
 };
 
 const setStatusActive = (button) => {
-  if (button.classList.contains("btn")) {
-    button.classList.add("btn-active");
+  if (button.classList.contains('btn')) {
+    button.classList.add('btn-active');
   }
 };
 
-fullScreen.addEventListener("mousedown", function () {
+fullScreen.addEventListener('mousedown', function () {
   if (document.documentElement.requestFullscreen) {
     document.documentElement.requestFullscreen();
   }
