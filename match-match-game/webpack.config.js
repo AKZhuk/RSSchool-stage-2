d16570fb55a8c868214da4e2fff994ef1e4b5ac8
Aliaksei Zhuk <lesha.zhuk@gmail.com>
2021-05-16 13:14:35 +0300

    init: start match-match-game task

---
+++ match-match-game/webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { web } = require('webpack');
+
+const esLintPlugin = (isDev) => (isDev ? [] : [new ESLintPlugin({ extensions: ['ts', 'js'] })]);
+
+const devServer = (isDev) =>
+  !isDev
+    ? {}
+    : {
+        devServer: {
+          open: true,
+          // hot: true,
+          port: 8080,
+          contentBase: path.join(__dirname, 'public'),
+        },
+      };
+
+module.exports = ({ development }) => ({
+  mode: development ? 'development' : 'production',
+  devtool: development ? 'inline-source-map' : false,
+  entry: './src/index.ts',
+
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[name][ext]',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Match-Match Game',
+      template: './src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    ...esLintPlugin(development),
+  ],
+  ...devServer(development),
+});
