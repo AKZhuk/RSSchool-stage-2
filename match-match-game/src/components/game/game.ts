8185ad3543fc614d96500d57bd24ab0c1d08aa03
Aliaksei Zhuk <lesha.zhuk@gmail.com>
2021-05-30 21:02:43 +0300

    fix: issue with click on active card, add reset of active cad and animation flag when game is starting

--- match-match-game/src/components/game/game.ts
+++ match-match-game/src/components/game/game.ts
@@ -31,29 +31,34 @@ export class Game extends BaseComponent {
     };
     this.timer = new Timer();
     this.gameField = new GameField();
+    this.modal = new Registration();
     this.element.appendChild(this.timer.element);
     this.element.appendChild(this.gameField.element);
-    this.modal = new Registration();
   }
 
-  newGame(images: string[], difficulty: number): void {
+  resetGame(): void {
     this.gameField.clear();
+    this.activeCard = undefined;
+    this.isAnimation = false;
     this.timer.clear();
-    this.timer.start();
     this.gameResult.mistakes = 0;
     this.gameResult.corrects = 0;
     this.gameResult.flips = 0;
+  }
+
+  newGame(images: string[], difficulty: number): void {
+    this.resetGame();
     this.gameResult.cardsPairs = images.length;
     const cards = images
       .concat(images)
-      .map((url) => new Card(url, difficulty))
+      .map(url => new Card(url, difficulty))
       .sort(() => Math.random() - 0.5);
     this.gameField.addCards(cards);
 
     setTimeout(() => {
-      cards.forEach((card) => {
+      this.timer.start();
+      cards.forEach(card => {
         card.element.addEventListener('click', () => {
-          console.log('listener', card);
           this.cardHandler(card);
         });
       });
@@ -66,7 +71,7 @@ export class Game extends BaseComponent {
   }
 
   private async cardHandler(card: Card) {
-    if (this.isAnimation || card.isCorrect) return;
+    if (this.isAnimation || card.isCorrect || card === this.activeCard) return;
     this.isAnimation = true;
 
     await card.flipToFront();
@@ -76,15 +81,17 @@ export class Game extends BaseComponent {
       this.isAnimation = false;
       return;
     }
+
     this.gameResult.flips++;
+
     if (this.activeCard.image !== card.image) {
       card.changeStatus('mistake');
       this.activeCard.changeStatus('mistake');
-      await delay(3000);
+      await delay(2000);
       await Promise.all([
         this.activeCard.changeStatus('mistake'),
-        this.activeCard.flipToBack(),
         card.changeStatus('mistake'),
+        this.activeCard.flipToBack(),
         card.flipToBack(),
       ]);
       this.gameResult.mistakes++;
