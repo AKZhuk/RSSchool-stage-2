4a2a050a04e04f23db5ccaf849664b7c32a0eb96
Aliaksei Zhuk <lesha.zhuk@gmail.com>
2021-05-31 20:54:09 +0300

    fix: issue when canvas  is drawn disproportionatel

--- match-match-game/src/shared/canvas.ts
+++ match-match-game/src/shared/canvas.ts
@@ -6,17 +6,20 @@ export class Canvas extends BaseComponent {
   constructor(style: string) {
     super('canvas', ['canvas', style]);
     this.element.setAttribute('width', '168');
-
     this.element.setAttribute('height', '168');
   }
 
   drawImage(src: string): void {
-    const img = new Image(168, 168);
+    const img = new Image();
     img.src = src;
-    img.setAttribute('style', 'object-fit:cover');
+    img.style.objectFit = 'cover';
     img.onload = () => {
       const ctx = <CanvasRenderingContext2D>(<HTMLCanvasElement>this.element).getContext('2d');
-      ctx.drawImage(img, 0, 0, img.width, img.height);
+      if (img.width > img.height) {
+        ctx.drawImage(img, 0, 0, img.width * (168 / img.height), 168);
+      } else {
+        ctx.drawImage(img, 0, 0, 168, img.height * (168 / img.width));
+      }
       this.base64Files = (this.element as HTMLCanvasElement).toDataURL();
     };
   }
