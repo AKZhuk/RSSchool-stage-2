13059dcd7e7ba8c78a3f52073b162e6791f88b88
Aliaksei Zhuk <lesha.zhuk@gmail.com>
2021-06-14 21:36:48 +0300

    refactor: state and winner

---
+++ async-race/src/state.ts
@@ -0,0 +1,14 @@
+import { State } from './interfaces';
+
+export const state: State = {
+  garagePage: 1,
+  winnerPage: 1,
+  sortBy: 'id',
+  orderBy: 'ASC',
+  selectedCarId: null,
+  amimation: {},
+  view: 'garage',
+  winner: null,
+  garagePagesCount: 0,
+  winnersPagesCount: 0,
+};
--- async-race/src/ui/winners.ts
+++ async-race/src/ui/winners.ts
@@ -1,14 +1,54 @@
-export const renderWinners = (): string => `
-<table class="table table-striped table-sm">
-  <thead>
+import {
+  getAllCars, getCar, getWinners, saveWinner,
+} from '../api';
+import { Car, Winner } from '../interfaces';
+import { state } from '../state';
+import { $ } from '../utils';
+import { renderCarImage } from './carImage';
+
+const renderWinner = (winner: Winner, car: Car, index: number): string => `
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">Car</th>
-      <th scope="col">Name</th>
-      <th scope="col" class="sort" data-sort="wins">Wins</th>
-      <th scope="col" class="sort" data-sort="time">Time</th>
+      <th scope="row">${index + 1}</th>
+      <td>${renderCarImage(car)}</td>
+      <td>${car.name}</td>
+      <td>${winner.wins}</td>
+      <td>${winner.time}</td>
     </tr>
-  </thead>
-  <tbody class="table-result">
-  </tbody>
-</table>`;
+`;
+
+export const renderWinners = async (): Promise<void> => {
+  const winners = await getWinners(
+    state.winnerPage,
+    10,
+    state.sortBy,
+    state.orderBy,
+  );
+  let index = 0;
+  const cars = await getAllCars();
+  state.winnersPagesCount = Math.ceil(Number(winners.count) / 10);
+  $('.winners__count').innerHTML = `Winners (${winners.count})`;
+  $('.winners__page').innerHTML = `Page #(${state.winnerPage})`;
+  $('.table-result').innerHTML = '';
+
+  winners.items.forEach(async (winner: Winner) => {
+    $('.table-result').innerHTML += renderWinner(
+      winner,
+      cars.items[winner.id],
+      index + (state.winnerPage - 1) * 10,
+    );
+    index++;
+  });
+};
+
+export const winnerHandler = async (winner: {
+  carId: number;
+  time: number;
+}): Promise<void> => {
+  const time = (winner.time / 1000).toFixed(2);
+  await saveWinner(winner.carId, time);
+  await getCar(winner.carId).then((response: Car) => {
+    $('.pop-up').innerHTML = `${response.name} went first (${time}s)!!`;
+  });
+  $('#resetRace').removeAttribute('disabled');
+  await renderWinners();
+};
