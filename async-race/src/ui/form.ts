2795a1403d60e16e7879a86ee4309a684e07ac98
Aliaksei Zhuk <lesha.zhuk@gmail.com>
2021-06-14 21:38:14 +0300

    feat: implement generate cars

--- async-race/src/ui/form.ts
+++ async-race/src/ui/form.ts
@@ -1,17 +1,10 @@
 export const renderForm = (): string => `
       <form id="formCreateCar" class="form-create input-group-sm">
-        <input
-          type="text"
-          class="form-control"
-          id="inputCarName "
-          placeholder="Car name"
-          required
-        />
-        <input
-          type="color"
-          class="form-control form-control-color"
-          id="ColorInput"
-          value="#563d7c"
+        <input type="text" class="form-control"
+          id="inputCarName" placeholder="Car name"
+          required/>
+        <input type="color" class="form-control form-control-color"
+          id="ColorInput" value="#563d7c"
           title="Choose your color"
           required
         />
@@ -20,23 +13,25 @@ export const renderForm = (): string => `
       <form id="formUpdateCar" class="form-create input-group-sm">
         <input
           type="text"
-          class="form-control mb-3"
+          class="form-control"
           id="updateCarName "
           placeholder="New car name"
           disabled
         />
         <input
           type="color"
-          class="form-control form-control-color mb-3"
+          class="form-control form-control-color"
           id="ColorUpdate"
           value="#563d7c"
           title="Choose your color"
           disabled
         />
-        <button type="submit" class="btn btn-primary mb-3" disabled>
+        <button type="submit" class="btn btn-primary" disabled>
           Update
         </button>
       </form>
       <div class = "btn-group"><button  id="startRace" type="button" class="btn btn-danger">Race</button>
-      <button  id="resetRace" type="button" class="btn btn-info">Reset</button></div>
+      <button  id="resetRace" type="button" class="btn btn-info" disabled>Reset</button>
+      <button  id="generateCars" type="button" class="btn btn-success">Generate Cars</button></div>
+
       `;
--- async-race/src/utils.ts
+++ async-race/src/utils.ts
@@ -1,30 +1,56 @@
-import { state } from '.';
-import { createWinner, drive, saveWinner, toggleEngine } from './api';
-import { Car, AnimateParam, Winner } from './interfaces';
-import { renderWinners } from './ui/winners';
+import {
+  createCar, drive, startEngine, toggleEngine,
+} from './api';
+import { Car, AnimateParam } from './interfaces';
+import { state } from './state';
 
-export const $ = (cls: string): HTMLElement =>
-  <HTMLElement>document.querySelector(cls);
+const carBrands: string[] = [
+  'Tesla',
+  'Porsche',
+  'Ferrari',
+  'BMW',
+  'Audi',
+  'VAZ',
+  'Mercedes',
+  'Lamborgini',
+  'Skoda',
+  'Tayota',
+];
+
+const carModels: string[] = [
+  '911',
+  'huracan',
+  'model S',
+  'm5',
+  'e-tron',
+  '2101',
+  'R8',
+  'Octavia RS',
+  'Camry 3.5',
+  'AMG',
+];
+
+export const $ = (cls: string): HTMLElement => <HTMLElement>document.querySelector(cls);
 
 export const disableForm = (): void => {
-  $('#formUpdateCar').children[0].setAttribute('disabled', '');
-  $('#formUpdateCar').children[1].setAttribute('disabled', '');
-  $('#formUpdateCar').children[2].setAttribute('disabled', '');
+  document.querySelectorAll('#formUpdateCar *').forEach((elem) => {
+    elem.setAttribute('disabled', '');
+  });
 };
 
 export const enableForm = (car: Car): void => {
   ($('#formUpdateCar').children[0] as HTMLInputElement).value = car.name;
   ($('#formUpdateCar').children[1] as HTMLInputElement).value = car.color;
-  $('#formUpdateCar').children[0].removeAttribute('disabled');
-  $('#formUpdateCar').children[1].removeAttribute('disabled');
-  $('#formUpdateCar').children[2].removeAttribute('disabled');
+  document.querySelectorAll('#formUpdateCar *').forEach((elem) => {
+    elem.removeAttribute('disabled');
+  });
 };
 
 export function animate({ carId, duration }: AnimateParam): void {
   const start = performance.now();
-  let animationReq: { id: number };
-
-  state.amimation[carId] = requestAnimationFrame(function animate(time): void {
+  state.amimation[carId] = requestAnimationFrame(function animateCar(
+    time,
+  ): void {
     let progress = (time - start) / duration;
     if (progress > 1) progress = 1;
 
@@ -33,25 +59,46 @@ export function animate({ carId, duration }: AnimateParam): void {
     }px`;
 
     if (progress < 1) {
-      state.amimation[carId] = requestAnimationFrame(animate);
+      state.amimation[carId] = requestAnimationFrame(animateCar);
     }
   });
 }
 
 export const race = async (carId: number) => {
-  const param = await toggleEngine(carId, 'started');
+  const param = await startEngine(carId);
+
   const time = param.distance / param.velocity;
   animate({
     duration: time,
     carId,
   });
   const { success } = await drive(carId);
+
   if (!success) {
     cancelAnimationFrame(state.amimation[carId]);
   }
   return { success, carId, time };
 };
 
-export const winnerHandler = async (winner) => {
-  saveWinner(winner.carId, (winner.time / 1000).toFixed(2));
+const generateColor = (): string => {
+  const letter = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letter[Math.floor(Math.random() * 15)];
+  }
+  return color;
+};
+
+export const generateCars = async (): Promise<void> => {
+  const createCarRequests: Promise<void>[] = [];
+  for (let i = 0; i < 100; i++) {
+    const brand = carBrands[Math.floor(Math.random() * carBrands.length)];
+    const model = carModels[Math.floor(Math.random() * carModels.length)];
+    const car: Car = {
+      name: `${brand} ${model}`,
+      color: generateColor(),
+    };
+    createCarRequests.push(createCar(car));
+  }
+  Promise.all(createCarRequests);
 };
